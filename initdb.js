c5aafbc6daaa6f6c019165432ea84dfc4a76c6a5
ashmit <ashmit.dudeja@gmail.com>
2025-06-16 13:29:02 +0530

    Added init db

--- initdb.js
+++ initdb.js
@@ -10,13 +10,16 @@ const pool = require('./config/db');
 async function initializeDatabase() {
   try {
     await pool.query(`
-      CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      );
+      CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255),
+  status VARCHAR(50),
+  isdeleted BOOLEAN DEFAULT false,
+  phone_number VARCHAR(20)
+);
+
     `);
 
     console.log("✅ Users table ensured");
